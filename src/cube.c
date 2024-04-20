1f4244879a4acfd2b19fc17146bf62fd32f34f89
Ashley Fletcher <asfletch@2-A-4.42heilbronn.de>
2024-04-20 10:08:56 +0200

    testing the origin and school script. No major changes made

--- src/cube.c
+++ src/cube.c
@@ -6,7 +6,7 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 08:21:46 by asfletch          #+#    #+#             */
-/*   Updated: 2024/04/20 09:22:13 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/04/20 10:08:21 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	main(int argc, char **argv)
 	init_map(cube);
 	fill_map(cube, argv[1]);
 	check_map_perimeter(cube);
-	//print_map(cube);
+	print_map(cube);
 	init_mlx(cube);
 	mlx_loop_hook(cube->mlx, hook, cube);
 	mlx_loop(cube->mlx);
