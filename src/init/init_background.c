c5c9dfe05ca57af35bfb294a8fe086e901644d1e
Muhammet Kose <muhakose@2-A-2.42heilbronn.de>
2024-04-21 14:54:19 +0200

    add black and white backround with better visualization

--- src/init/init_background.c
+++ src/init/init_background.c
@@ -6,7 +6,7 @@
 /*   By: muhakose <muhakose@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 13:10:35 by asfletch          #+#    #+#             */
-/*   Updated: 2024/04/21 14:47:41 by muhakose         ###   ########.fr       */
+/*   Updated: 2024/04/21 14:52:53 by muhakose         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,21 @@ void	background(void *param)
 	uint32_t		x;
 	uint32_t		y;
 	t_cube			*cube;
-
 	cube = (t_cube *)param;
+	int		i = cube->map_size;
+
 	x = 0;
 	while (x < cube->image->width)
 	{
 		y = 0;
 		while (y < cube->image->height)
 		{
-			if (cube->map->map[y / cube->map_size][x / cube->map_size] == '1')
+			if (cube->map->map[y / i][x / i] == '1' && x % i != 0 && y % i != 0)
 				mlx_put_pixel(cube->image, x, y, pixel(255, 255, 255, 255));
-			else
+			else if (x % i != 0 && y % i != 0)
 				mlx_put_pixel(cube->image, x, y, pixel(0, 0, 0, 255));
+			else
+				mlx_put_pixel(cube->image, x, y, pixel(0, 0, 0, 0));
 			y++;
 		}
 		x++;
