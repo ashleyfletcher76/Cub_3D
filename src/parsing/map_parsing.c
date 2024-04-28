560259ea381997aa6e81486354bde0d051768e7b
Ashley Fletcher <asfletch@3-C-7.42heilbronn.de>
2024-04-28 10:09:08 +0200

    Small update so my partner has the latest

--- src/parsing/map_parsing.c
+++ src/parsing/map_parsing.c
@@ -6,7 +6,7 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 10:43:38 by asfletch          #+#    #+#             */
-/*   Updated: 2024/04/27 17:26:03 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/04/28 10:08:39 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ t_cube	parse_map(t_cube *cube, char *map)
 		free (line);
 		line = get_next_line(fd);
 	}
-	convert_details(cube);
+	//convert_compare(cube);
 	close (fd);
 	return (*cube);
 }
